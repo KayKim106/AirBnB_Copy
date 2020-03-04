5b06cdba4cb878277f5e3df3213f59af3591403c
Kay Kim <kkim@threeon.io>
2020-03-03 23:26:23 -0500

    add explore ui design

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, SafeAreaView, TextInput, Platform, StatusBar } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Platform,
+  StatusBar,
+  ScrollView,
+  Image,
+  Dimensions,
+} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+import Category from "../components/Explore/Category";
 
+
+
+
+const { height, width} = Dimensions.get('window');
 class Explore extends Component {
   componentWillMount() {
     this.startHeaderHeight = 80;
-    if(Platform.OS === "android"){
-        this.startHeaderHeight = 100 + StatusBar.currentHeight
+    if (Platform.OS === "android") {
+      this.startHeaderHeight = 100 + StatusBar.currentHeight;
     }
-}
+  }
 
   render() {
     return (
@@ -33,7 +49,7 @@ class Explore extends Component {
                 shadowOpacity: 0.2,
                 elevation: 1,
                 marginTop: Platform.OS === "android" ? 30 : null
-              }}  
+              }}
             >
               <Icon name="ios-search" size={20} style={{ marginRight: 10 }} />
               <TextInput
@@ -44,6 +60,49 @@ class Explore extends Component {
               />
             </View>
           </View>
+          <ScrollView scrollEventThrottle={16}>
+            <View style={{ flex: 1, backgroundColor: "white", paddingTop: 20 }}>
+              <Text
+                style={{
+                  fontSize: 24,
+                  fontWeight: "700",
+                  paddingHorizontal: 20
+                }}
+              >
+                {" "}
+                What can we help you find, Varun?
+              </Text>
+              <View style={{ height: 130, marginTop: 20 }}>
+                <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false} >
+                  <Category
+                    name="first"
+                    imageUri={require("../../assets/travel1.jpg")}
+                  />
+                  <Category
+                    name="Experience"
+                    imageUri={require("../../assets/travel1.jpg")}
+                  />
+                  <Category
+                    name="Restaurant"
+                    imageUri={require("../../assets/travel1.jpg")}
+                  />
+                </ScrollView>
+              </View>
+              <View style={{ marginTop:40, paddingHorizontal:20}}>
+                  <Text style={{fontSize:24, fontWeight:"700"}}>
+                      Introducing AirBnb 
+                  </Text>
+                  <Text style={{fontWeight:"100", marginTop:10}}>
+                      A New Selection of homes verified for quality & comfort
+                  </Text>
+                  <View style={{width:width-40, height:200, marginTop:20}}>
+                      <Image style={{flex:1, height:null, width:null, resizeMode:"cover", borderRadius:5, borderWidth:1, borderColor:"#dddddd" }} source={require('../../assets/travel1.jpg')} />
+                  </View>
+              </View>
+            </View>
+          </ScrollView>
         </View>
       </SafeAreaView>
     );
